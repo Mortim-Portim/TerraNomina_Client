16c09f90a2abe1cdb329462913ebfdd382e609f6
BuildTools <unconfigured@null.spigotmc.org>
2020-11-07 00:49:52 +0100

    Race Selection Added

--- Game/characterMenu.go
+++ Game/characterMenu.go
@@ -22,16 +22,43 @@ type CharacterMenu struct {
 }
 
 func getRace(race *Race, x, y float64) (group *GE.Group) {
-	title := GE.GetTextImage(race.name, x+300, y+50, 50, GE.StandardFont, color.Black, &color.RGBA{168, 255, 68, 255})
-	group = GE.GetGroup(title)
+	title := GE.GetTextImage(race.name, x+400, y+50, 50, GE.StandardFont, color.Black, &color.RGBA{168, 255, 68, 255})
+	stats := GE.GetTextImage(fmt.Sprintf("STR: %v DEX: %v INT: %v CHA: %v", race.attributes[0], race.attributes[1], race.attributes[2], race.attributes[3]), x+480, y+160, 30, GE.StandardFont, color.Black, &color.RGBA{168, 255, 68, 255})
+
+	subraces := make([]GE.UpdateAble, len(race.subraces))
+	for i, subrace := range race.subraces {
+		subraces[i] = GE.GetTextImage(subrace, x+220, y+160+float64(i*40), 30, GE.StandardFont, color.Black, &color.RGBA{168, 255, 68, 255})
+	}
+
+	group = GE.GetGroup(append(subraces, title, stats)...)
 	return
 }
 
 func (menu *CharacterMenu) Init(g *TerraNomina) {
 	rx, ry := 100.0, 100.0
 
-	racebackground, _ := GE.LoadImgObj(F_CHARACTERMENU+"/racetemplate.png", 700, 500, rx, ry, 0)
-	menu.racething = GE.GetGroup(racebackground)
+	racebackground, _ := GE.LoadImgObj(F_CHARACTERMENU+"/racetemplate.png", 0, 0, rx, ry, 0)
+	racebackground.ScaleToOriginalSize()
+
+	lbuttonimg, _ := GE.LoadImgObj(F_CHARACTERMENU+"/arrow.png", 0, 0, 20, 300, 0)
+	lbuttonimg.ScaleToOriginalSize()
+	leftbuton := GE.GetButton(lbuttonimg, lbuttonimg.Img)
+	leftbuton.RegisterOnLeftEvent(func(btn *GE.Button) {
+		if btn.LPressed {
+			menu.ChangeRace(-1)
+		}
+	})
+
+	rbuttonimg, _ := GE.LoadImgObj(F_CHARACTERMENU+"/arrow.png", 0, 0, 1010, 300, 180)
+	rbuttonimg.ScaleToOriginalSize()
+	rightbuton := GE.GetButton(rbuttonimg, rbuttonimg.Img)
+	rightbuton.RegisterOnLeftEvent(func(btn *GE.Button) {
+		if btn.LPressed {
+			menu.ChangeRace(1)
+		}
+	})
+
+	menu.racething = GE.GetGroup(racebackground, leftbuton, rightbuton)
 	menu.racething.Init(nil, nil)
 
 	for _, race := range Races {
@@ -60,12 +87,28 @@ func (menu *CharacterMenu) Update(screen *ebiten.Image) error {
 	menu.racething.Update(0)
 	menu.races[menu.currRace].Update(0)
 
+	screen.Fill(color.RGBA{128, 128, 128, 255})
+
 	menu.racething.Draw(screen)
-	menu.races[menu.currRace].DrawFuncs[0](screen)
+	menu.races[menu.currRace].Draw(screen)
 
 	return nil
 }
 
 func (menu *CharacterMenu) GetBack() {
 	menu.parent.ChangeState(TITLESCREEN_STATE)
+}
+
+func (menu *CharacterMenu) ChangeRace(delta int) {
+	menu.currRace += delta
+
+	if menu.currRace < 0 {
+		menu.currRace = len(menu.races) - 1
+	}
+
+	if menu.currRace >= len(menu.races) {
+		menu.currRace = 0
+	}
+
+	fmt.Printf("Changed to %v \n", menu.currRace)
 }
--- Game/race.go
+++ Game/race.go
@@ -3,9 +3,12 @@ package Game
 var Races []*Race
 
 func InitRaces() {
-	elv := &Race{"Elv", 0, []int8{0, 2, 2, 0}, []string{"Wood Elv", "High Elv"}}
+	human := &Race{"Human", 0, []int8{1, 1, 1, 1}, []string{"Urban Human", "Country-Side Human", "Mountain Tribe"}}
+	elv := &Race{"Elv", 1, []int8{0, 2, 2, 0}, []string{"Wood Elv", "High Elv"}}
+	halfelv := &Race{"Half-Elv", 2, []int8{0, 2, 0, 2}, []string{"Dark Elv", "City Elv"}}
+	ork := &Race{"Ork", 3, []int8{3, 0, 0, 0}, []string{"Mountain Ork", "Cave Ork"}}
 
-	Races = []*Race{elv}
+	Races = []*Race{human, elv, halfelv, ork}
 }
 
 type Race struct {
